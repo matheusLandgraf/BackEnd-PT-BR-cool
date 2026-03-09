b0d2d01de4b337d5a06de59134c5ab18d239483b
MatheusLandgraf <matheus.henriquelandgrafcanedode.melo@escola.pr.gov.br>
2026-03-09 20:38:04 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const db = require("./db")
-    console.log("começou");
-    console.log("select * from produtos");
-    const produtos = await db.selectprodutos();
-    console.log(produtos);
-})();
